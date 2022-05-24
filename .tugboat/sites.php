0e3b069f90f9d8af370d4c48a73f984b271f7c2c
Mike Herchel <mike@herchel.com>
2022-05-24 10:13:09 -0400

    Update URL in sites.php

--- .tugboat/sites.php
+++ .tugboat/sites.php
@@ -4,12 +4,12 @@
 $sites['localhost'] = 'preloaded';
 $sites['preloaded'] = 'preloaded';
 $sites[getenv('TUGBOAT_DEFAULT_SERVICE_URL_HOST')] = 'preloaded';
-$sites['preloaded-' . getenv('TUGBOAT_DEFAULT_SERVICE_TOKEN') . '.tugboat.qa'] = 'preloaded';
+$sites['preloaded-' . getenv('TUGBOAT_DEFAULT_SERVICE_TOKEN') . '.tugboatqa.com'] = 'preloaded';
 
 // Domain mappings for minimal.
 $sites['minimal'] = 'minimal';
-$sites['minimal-' . getenv('TUGBOAT_DEFAULT_SERVICE_TOKEN') . '.tugboat.qa'] = 'minimal';
+$sites['minimal-' . getenv('TUGBOAT_DEFAULT_SERVICE_TOKEN') . '.tugboatqa.com'] = 'minimal';
 
 // Domain mappings for standard.
 $sites['standard'] = 'standard';
-$sites['standard-' . getenv('TUGBOAT_DEFAULT_SERVICE_TOKEN') . '.tugboat.qa'] = 'standard';
+$sites['standard-' . getenv('TUGBOAT_DEFAULT_SERVICE_TOKEN') . '.tugboatqa.com'] = 'standard';
